c731757124e099f449859c10a4485b510384882c
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-01-19 10:33:13 -0300

    requisito 5

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private _energyType: EnergyType;
+  private static intancesCreated = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Necromancer.addInstance();
+  }
+
+  private static addInstance() {
+    this.intancesCreated += 1;
+  }
+
+  public static createdArchetypeInstances(): number {
+    return this.intancesCreated;
+  }
+
+  public get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
